449a3dc0097b9a1f2af41fd6a2c04cad2f17aaab
krishan <bagda909@gmail.com>
2024-03-15 11:47:02 +0530

    firt commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './vendor/bundle/ruby/2.7.0/src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
